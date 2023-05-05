cd35380dd2449625c432898a6431045fb57e9932
laurus <1852149514@qq.com>
2023-05-05 16:38:13 +0800

    新写的一个算法

---
+++ offer/src/_删除链表的节点.java
@@ -0,0 +1,20 @@
+public class _删除链表的节点 {
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head.val == val){
+            return head.next;
+        }
+        ListNode pre = head;
+        ListNode cur = head;
+        while (cur!=null){
+            if(cur.val == val){
+                pre.next = cur.next;
+                break;
+            }
+            pre = cur;
+            cur = cur.next;
+        }
+        return head;
+    }
+
+}
